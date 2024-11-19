b6c71af58482f820ed73442191e523b59a0cdac3
imrahul05 <rahulkumar20000516@gmail.com>
2024-11-19 12:50:01 +0530

    Enhance Git instructions and styling; add copy buttons and improve CSS for better readability

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        alert('Copied to clipboard');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+}
+</script>
